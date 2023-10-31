382c9472fb4dca45b74379d8b0e1bdd8338bf3e0
Chris Phillips <chris.phillips@mindmatrix.net>
2023-10-31 13:10:47 -0400

    added helm and kuebctl to ansible so that all config happens here

--- init-ansible.sh
+++ init-ansible.sh
@@ -9,9 +9,15 @@ GITHUB_USER=$1
 
 # ubuntu doesn't have the latest ansible, need to use ppa
 sudo apt update
-sudo apt install software-properties-common -y
+sudo apt install software-properties-common apt-transport-https ca-certificates curl -y
 sudo add-apt-repository --yes --update ppa:ansible/ansible
-sudo apt install ansible git vim python3-netaddr -y
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.28/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+curl https://baltocdn.com/helm/signing.asc | gpg --dearmor | sudo tee /usr/share/keyrings/helm.gpg > /dev/null
+sudo apt-get install apt-transport-https --yes
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/helm.gpg] https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
+sudo apt-get update
+sudo apt install ansible git vim python3-netaddr kubectl helm -y
 
 # we need to generate a key for github auth on the server cluster
 # check out the 2 repos to configure VM + k3s
